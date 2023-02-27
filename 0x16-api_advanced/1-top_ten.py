7425c0142bd526caf1cdf986e85520c3aea28f06
IVashty <vashtylavilavi50@gmail.com>
2023-02-27 15:45:54 +0300

    1-top_ten.py

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+fucntion that queries the REDDIT api & prints \
+        the title of the first 10 hot posts listed from a given subreddit
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """
+    prints the titles of the first 10 hot posts listed for the given subreddit.
+    Args:
+        subreddit(str):subreddit to query.
+    """
+    user_agent = "My User Agent"
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {"User-Agent": user_agent}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        for post in data["data"]["children"]:
+            print(post["data"]["title"])
+    else:
+        print("None")
