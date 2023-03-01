df68eb1f9f1885cae0ade453e5ca7b3abc6e3871
IVashty <vashtylavilavi50@gmail.com>
2023-03-01 15:09:54 +0300

    2-recurse.py

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+recursive function that queries REDDIT api returning \
+        a list containing the title of all hot articles given subreddit
+"""
+
+import requests
+
+def recurse(subreddit, hot_list=[]):
+    #Check valid subreddit
+    url = "https://www.reddit.com/r/" + subreddit + "/hot.json"
+    response = requests.get(url, headers = {'User-agent': 'programming'})
+    if response.status_code == 200:
+        response_dict = response.json()
+        hot_posts = response_dict["data"]["children"]
+        for post in hot_posts:
+            hot_list.append(post["data"]["title"])
+        #Check for paginated results
+        if response_dict["data"]["after"] is not None:
+            recurse(subreddit, hot_list, response_dict["data"]["after"])
+        else:
+            return hot_list
+    else:
+        return None
